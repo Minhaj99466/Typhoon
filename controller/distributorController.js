a867c58915d2aef812fd58a23adee9fed58058e5
anasachu2000 <muhammedanasea2000@gmail.com>
2024-05-26 16:46:07 +0530

    order list completed

--- controller/distributorController.js
+++ controller/distributorController.js
@@ -24,6 +24,7 @@ const loadOrderDetails = async(req,res) =>{
               },
             },
           ]);
+          console.log(orderData);
         res.render("order",{orderData})
     } catch (error) {
         console.log(error)
